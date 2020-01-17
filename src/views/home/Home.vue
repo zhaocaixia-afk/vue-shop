28bd5807552ff865d2f4852b25d468323c11d969
赵采霞 <121357625@qq.com>
2020-01-17 09:56:39 +0800

    Home中的el-header部分

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,7 +1,19 @@
 <template>
-    <div>
-        <el-button type="info" @click="logout">退出</el-button>
-    </div>
+    <el-container class="home-container">
+        <el-header>
+            <div>
+                <img src="~assets/img/heima.png"/>
+                <span>电商后台管理系统</span>
+            </div>
+            <el-button type="info" @click="logout">退出</el-button>
+        </el-header>
+        <el-container>
+            <el-aside width="260px">
+                <!-- 侧边栏菜单 -->
+            </el-aside>
+            <el-main>Main</el-main>
+        </el-container>
+    </el-container>
 </template>
 
 <script>
@@ -18,5 +30,34 @@ import { clearSession } from 'common/utils'
 </script>
 
 <style lang="scss" scoped>
-
+.home-container{
+    height: 100%;
+    .el-header{
+        background-color: #373d41;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        div{
+            display: flex;
+            align-items: center;
+            img{
+                width: 50px;
+                height: 50px;
+            }
+            span{
+                color: #fff;
+                font-size: 16px;
+                margin-left: 10px;
+            }
+        }
+    }
+    .el-container{
+        .el-aside{
+            background-color: #333744;
+        }
+        .el-main{
+            background-color: #eaedf1;
+        }
+    }
+}
 </style>
